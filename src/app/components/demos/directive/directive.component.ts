d66c1f592c3339021b1e3385b9b4a13015a83984
Caven <34606118+Caven15@users.noreply.github.com>
2024-10-08 14:43:07 +0200

    directives

--- src/app/components/demos/directive/directive.component.ts
+++ src/app/components/demos/directive/directive.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './directive.component.scss'
 })
 export class DirectiveComponent {
+    visible : boolean = true
+    options : string | null = null
+    fruits : string[] = ["Pomme", "Poire", "Pêche"]
+    estSpecial : boolean = true
+    peutValider : boolean = false
+
+    toggle() : void{
+        this.visible = !this.visible
+        console.log(this.visible);
+    }
+
+    selection(optionSlectionnee : string): void{
+        this.options = optionSlectionnee
+    }
+
+    reset() : void {
+        this.options = null
+    }
 
 }
